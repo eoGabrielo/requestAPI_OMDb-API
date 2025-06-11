a8c58a2182899800aef16445f67ab5c1a1786d99
Gabriel José <105285297+eoGabrielo@users.noreply.github.com>
2025-06-11 04:57:17 -0300

    Update main.py
    final code

--- main.py
+++ main.py
@@ -13,16 +13,21 @@ response = requests.get(url)
 data = response.json()
 
 #If the request is completed with code 200
-if response.status_code == 200:
+if response.status_code == 200 and data.get('Title') == None:
+    print("Movie not found ")
+elif response.status_code == 200:
     print("Connection 200 successful")
-    #verify json - print(f"{data}")
+    # verify json - print(f"{data}")
     print(f"Title: {data.get('Title')}")
     print(f"year: {data.get('Year')}")
     print(f"Director: {data.get('Director')}")
     print(f"Gender: {data.get('Genre')}")
     print(f"Score IMDb: {data.get('imdbRating')}")
-else:
-    print(f"Error request code: {response.status_code}")
+elif response.status_code == 401:
+    print(f"Error request code: {response.status_code} Unauthorized")
+elif response.status_code == 504:
+    print(f"Error request code: {response.status_code} server error")
+
 
 #Data movie JSON
 dataMovie = {
@@ -33,12 +38,15 @@ dataMovie = {
         'Score IMDb': data.get('imdbRating')
 }
 
-# name file
-nameFile = 'movie.txt'
+#Set name file
+nameFile = f"{dataMovie['Title']}.txt"
 
 # Write in Txt
-with open(nameFile, mode='w', encoding='utf-8') as f:
-    for key, value in dataMovie.items():
-        f.write(f"{key}: {value}")
-        f.write('\n')
-print(f'Date saved in {data.get("Title")} in file {nameFile} in this folder')
+if response.status_code == 200 and nameFile != None:
+    with open(nameFile, mode='w', encoding='utf-8') as f:
+        for key, value in dataMovie.items():
+            f.write(f"{key}: {value}")
+            f.write('\n')
+        print(f'Date saved in {data.get("Title")} in file movie.txt in this folder')
+
+
