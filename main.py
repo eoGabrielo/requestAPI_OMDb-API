bbd8b1bdce8ecf8a782a3401ebf3a10a1598840b
Gabriel José <105285297+eoGabrielo@users.noreply.github.com>
2025-06-12 11:35:31 -0300

    Update main.py
    Adicionado funções e reorganização

--- main.py
+++ main.py
@@ -1,56 +1,51 @@
+import pandas as pd
 import requests
-import pandas as pandas
+
 #API key to link registration
 API_KEY = '91fbd453'
 
-movieTitle = input("Enter movie name: ")
+titleMovie = input('Write title movie: ')
 
-#URL-API with your key and title
-url = f'http://www.omdbapi.com/?apikey={API_KEY}&t={movieTitle}'
-#request API
-response = requests.get(url)
-#Convert requst in json
-data = response.json()
+def searchMovie(title):
+    # URL-API with your key and title
+    url = f'http://www.omdbapi.com/?apikey={API_KEY}&t={title}'
 
-#Data movie JSON
-dataMovie = {
-        'Title': data.get('Title'),
-        'year': data.get('Year'),
-        'Director': data.get('Director'),
-        'Gender:': data.get('Genre'),
-        'Score IMDb': data.get('imdbRating')
-}
+    # request API, check StatusCode
+    response = requests.get(url)
+    #print(f"RESPONSE -> {response}")
 
-#If the request is completed with code 200
-if response.status_code == 200 and data.get('Title') == None:
-    print("Movie not found ")
-    #Set name to file if Title == None
-    nameFile = None
-elif response.status_code == 200:
-    # Convert json in DataFrama to table
-    dataframe = pandas.DataFrame(data)
-    # Create dataframe table in Excel
-    dataframe.to_excel(f"{dataMovie['Title']}.xlsx")
-    # Set name to file csv
-    nameFile = f"{dataMovie['Title']}.csv"
-    print("")
-    print("Connection 200 successful...\n")
-    # verify json - print(f"{data}")
-    print(f"Title: {data.get('Title')}")
-    print(f"year: {data.get('Year')}")
-    print(f"Director: {data.get('Director')}")
-    print(f"Gender: {data.get('Genre')}")
-    print(f"Score IMDb: {data.get('imdbRating')}\n")
-    print(dataframe)
-elif response.status_code == 401:
-    print(f"Error request code: {response.status_code} Unauthorized")
-elif response.status_code == 504:
-    print(f"Error request code: {response.status_code} server error")
+    # Code Status
+    statusCode = response.status_code
+    #print(f"{statusCode}")
 
+    # Convert to JSON
+    dataJson = response.json()
+    if response.status_code == 200 and dataJson.get('Title') == None:
+        print(f"Movie not found, are you sure that's the name? -> {title} ")
+        return None
+    elif response.status_code == 401:
+        print(f"Error request code: {response.status_code} Unauthorized")
+        return None
+    elif response.status_code == 504:
+        print(f"Error request code: {response.status_code} server error")
+        return None
+    elif response.status_code == 200:
+        print(f"EXCEL and CSV file created in this folder with successfully, named as {dataJson.get('Title')}.xlsx/csv")
+    return dataJson
 
-#Create file and Write in Csv if the title returns correctly
-if response.status_code == 200 and nameFile != None:
-    with open(nameFile, mode='w', encoding='utf-8') as f:
-        for key, value in dataMovie.items():
+#Saved return JSON for convert to DATAFRAME and CSV
+dataJsonMovie = searchMovie(titleMovie)
+
+#Convert JSON in DATAFRAME
+dataFrame = pd.DataFrame(dataJsonMovie)
+
+def createCsvFile(dfMovie):
+    with open(f"{dfMovie['Title']}.csv", mode='w', encoding='utf-8') as f:
+        for key, value in dfMovie.items():
             f.write(f"{key}: {value}\n")
-        print(f'Table and CSV of Date of {data.get("Title")} saved in file {data.get("Title")}.xlsx/csv in this folder\n')
+
+#Create table with DATAFRAME
+if dataJsonMovie != None:
+    #Create Excel file
+    dataFrame.to_excel(f"{dataJsonMovie['Title']}.xlsx")
+    createCsvFile(dataJsonMovie)
