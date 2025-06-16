e0ba505938ed404e0a08fe4a326d7085250598cd
Gabriel José <105285297+eoGabrielo@users.noreply.github.com>
2025-06-15 22:52:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,3 @@ dados_Do_Filme_JSON = buscar_Filme_Retorna_JSON(tituloFilme)
 if dados_Do_Filme_JSON != None:
      criar_DataFrame_Excel(dados_Do_Filme_JSON)
      criarCSV(dados_Do_Filme_JSON)
-
-
-=======
-from funcs import buscar_Filme_Retorna_JSON, criarCSV, criar_DataFrame_Excel
-
-tituloFilme = input('Escreva o nome de um filme:')
-
-dados_Do_Filme_JSON = buscar_Filme_Retorna_JSON(tituloFilme)
-
-if dados_Do_Filme_JSON != None:
-     criar_DataFrame_Excel(dados_Do_Filme_JSON)
-     criarCSV(dados_Do_Filme_JSON)
