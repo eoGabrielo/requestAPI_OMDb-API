7f6895b992ff8c0be7694141b7626637b196ca6f
Gabriel José <105285297+eoGabrielo@users.noreply.github.com>
2025-06-15 22:53:00 -0300

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import requests
 import pandas as pd
 
@@ -32,39 +31,4 @@ def criar_DataFrame_Excel(dadosJson):
 def criarCSV(dadosJson):
     with open('arquivos/texto.csv', mode='w', encoding='utf-8') as f:
         for key, value in dadosJson.items():
-=======
-import requests
-import pandas as pd
-
-# API key to link registration
-chave = '91fbd453'
-
-def buscar_Filme_Retorna_JSON(titulo):
-    #Url da API para digitar chave e titulo do filme
-    url = f'http://www.omdbapi.com/?apikey={chave}&t={titulo}'
-
-    #Resposta e requisiçao
-    response = requests.get(url)
-    json =  response.json()
-    codigoDeStatus = response.status_code
-
-    if codigoDeStatus == 200:
-        print(f"Filme [{json.get('Title')}], buscado com sucesso, veja seus dados na pasta arquivos!")
-        return json
-    elif codigoDeStatus == 401:
-        print("Não autorizado, Erro 401!")
-        return None
-    elif codigoDeStatus == 504:
-        print("Erro no servidor, Erro 504!")
-        return None
-
-def criar_DataFrame_Excel(dadosJson):
-    dataFrame = pd.DataFrame(dadosJson)
-    dataFrame.to_excel('arquivos/Tabela.xlsx')
-
-
-def criarCSV(dadosJson):
-    with open('arquivos/texto.csv', mode='w', encoding='utf-8') as f:
-        for key, value in dadosJson.items():
->>>>>>> 9f51d3926c0496b71156d94b52ad834cdbd9945e
             f.write(f"{key}: {value}\n")
